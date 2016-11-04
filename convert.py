2f69ea7d1039b6f212154daa16a5e65557973514
AteamSF <ibhargav92@gmail.com>
2016-11-03 18:16:17 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -10,15 +10,15 @@ print n.reshape(3,3,3)
 #creating numpy arrays out of images
 # 0 represents importing gray scale image 
 def im_gray(x):
-	x = cv2.imread("C:/Users/ibhar/OneDrive/Pictures/smallgray.png",0)
+	x = cv2.imread("C:/Users/ibhar/OneDrive/Pictures/gray.png",0)
 
-x = cv2.imread("C:/Users/ibhar/OneDrive/Pictures/smallgray.png",0)
+x = cv2.imread("C:/Users/ibhar/OneDrive/Pictures/gray.png",0)
 print im_gray(x)
 
 # 1 represents a color image i.e, BGR
 # openCV uses BGR format and NOT RGB! 
-im_gray = cv2.imread("C:/Users/ibhar/OneDrive/Pictures/smallgray.png",1)
+im_gray = cv2.imread("C:/Users/ibhar/OneDrive/Pictures/gray.png",1)
 print im_gray
 
 #creating images out of numpy arrays
-print cv2.imwrite("newsmallgray.png",im_gray)
+print cv2.imwrite("newgray.png",im_gray)
